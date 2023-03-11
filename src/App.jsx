bab80f672f4bdebf79974abcc8ac26280afab21c
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-11 20:35:15 +0000

    Install Firebase Install Firebase and create a base sign in component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 export const Shop = () => {
 	return <div>I'm a shopping page</div>;
@@ -13,6 +14,7 @@ const App = () => {
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<Home />} />
 				<Route path="shop" element={<Shop />} />
+				<Route path="signin" element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
