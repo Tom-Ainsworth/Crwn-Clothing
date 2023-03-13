6b50641705dfb61a8fcdce994c7be1290cd6f8ed
Tom Ainsworth <tomainsworth94@gmail.com>
2023-03-13 17:53:31 +0000

    Add Firestore db
    Add functionality to create a user via google sign in, and save that to a firestore users collection on successful sign in

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
 	signInWithPopup,
 	GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBs4iSiYXuUpnjjDIUafdRHQOFroz4SwXQ",
@@ -25,3 +26,34 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "users", userAuth.uid);
+
+	console.log(userDocRef);
+
+	const userSnapshot = await getDoc(userDocRef);
+
+	console.log(userSnapshot);
+	console.log(userSnapshot.exists());
+
+	// if user data exists
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		// set doc with user auth data
+		try {
+			await setDoc(userDocRef, {
+				displayName: displayName,
+				email: email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log("Error creating user: ", error);
+		}
+	}
+	return userDocRef;
+};
